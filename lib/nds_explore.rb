edbec625bf79ee80b846375c3fc415bfbe516450
Tomas Tobar <codetombomb@gmail.com>
2019-12-06 00:39:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -37,24 +37,7 @@ end
 #     :worldwide_gross=>182207973,
 #     :release_year=>2012}]},
 
-# Hello again! I've hit a wall and my start date of this Monday depends on me getting through with Hashketball so I need get through this one I've been stuck on for the past day and a half lol. Here's what I've got
 
-# 1)I needed to set a parameter for the following method to take in the argument of directors_database so that I could access it inside of the method. I did that with:
-
-#print_first_directors_movie_title(directors_database)
-
-# 2) I needed to access the Spielberg hashes with key name :movies to iterate over -- my Solution -- nds[0][:movies] gives access to array of movie hashes.
-# nds[0][:movies] .length = 6 (the number of movies). Iterating over rows with ‘while’ statement
-
-# 3) I needed to Access the hash rows and iterate over
-# 	--Solution --
-# while element_index < nds[0][:movies][element_index].length do end
-# end
-
-# 4) I need to access each key in the hash to see if it is :title
-# ################ this is where I'm stuck. I think I need to use a method to iterate over the row?? Currently, Im trying to impliment this:
-
-# hash.each_key { |key| block }
 
 
 
